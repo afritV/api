b3473220f12e03dc226233ecba6c3ecf306b4eab
VladOleinik <vlad.o@kultprosvet.net>
2021-11-15 14:18:44 +0200

    add vue crud

---
+++ resources/js/components/App.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light text-center">
+            <div class="container">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <router-link class="nav-link" :to="{name:'home'}">Home</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link class="nav-link" :to="{name:'desks'}">Desks</router-link>
+                    </li>
+
+                </ul>
+            </div>
+    </nav>
+    <div>
+        <router-view></router-view>
+
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
